69b184651904cd63c8ff7861516dbb06e62bb881
Pedro Cazala <cazalapedro@gmail.com>
2025-12-30 12:09:00 -0300

    imagenes servidas desde mi sitio

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+    domains: ['pedritoenmovimiento.com.ar'],
+  },
 };
 
 export default nextConfig;
